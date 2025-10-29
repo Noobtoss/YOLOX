2f4b25a0c375ea231f8563fb0ee50d27f9a9a48c
Noobtoss <thomasschmittba17196@yahoo.de>
2025-10-29 14:45:53 +0100

    udpated export_coreml to make its nms class_agnostic

--- custom/python_scripts/export_coreml.py
+++ custom/python_scripts/export_coreml.py
@@ -137,7 +137,7 @@ def main():
     nms.confidenceThresholdInputFeatureName = "confidenceThreshold"
     nms.iouThreshold = 0.5
     nms.confidenceThreshold = 0.4
-    nms.pickTop.perClass = True
+    nms.pickTop.perClass = False  # True  # THS: makes nms class agnostic
     nms.stringClassLabels.vector.extend(names.values())
     nms_model = ct.models.MLModel(nms_spec)
 
