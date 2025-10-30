8d379bd2b365df35a23026590e019cc9abd749c4
Noobtoss <thomasschmittba17196@yahoo.de>
2025-10-30 13:48:42 +0100

    experiment

--- custom/exps/adapts/ams_yolo_head.py
+++ custom/exps/adapts/ams_yolo_head.py
@@ -123,7 +123,7 @@ class YOLOXHead(nn.Module):
         self.use_l1 = False
         self.l1_loss = nn.L1Loss(reduction="none")
         self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
-        self.ams_loss = AMSoftmaxLoss(embedding_dim=320, no_classes=num_classes, reduction="none")
+        self.ams_loss = AMSoftmaxLoss(embedding_dim=320, no_classes=num_classes, scale=10.0, reduction="none")
         self.iou_loss = IOUloss(reduction="none")
         self.strides = strides
         self.grids = [torch.zeros(1)] * len(in_channels)
