e639c86fc6b0fde9ce473bc37dce5b59ac035718
Noobtoss <thomasschmittba17196@yahoo.de>
2026-02-09 13:53:10 +0100

    renamed folder struct

--- overrides/slurm/slurm_train_arr.sh
+++ overrides/slurm/slurm_train_arr.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=1          # CPU Kerne pro Task (>1 für multi-threaded Tasks)
 #SBATCH --mem-per-cpu=64G          # RAM pro CPU Kern #20G #32G #64G
 
-BASE_DIR=/nfs/scratch/staff/schmittth/code-nexus/YOLOX
+BASE_DIR=/nfs/scratch/staff/schmittth/code_nexus/YOLOX
 # CFG=${1:-overrides/exps/Images04.py}
 # CKPT=${2:-checkpoints/yolox_x.pth}
 
