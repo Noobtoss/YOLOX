6d51fa73280b359fc3867f8b6ad22612e45bdcbf
Noobtoss <thomasschmittba17196@yahoo.de>
2026-05-21 11:39:51 +0200

    update

--- custom/slurm/alex/slurm_train_arr.sh
+++ custom/slurm/alex/slurm_train_arr.sh
@@ -34,7 +34,7 @@ OUTPUT_DIR="${BASE_DIR}/runs"
 EXP_NAME="${KV[exp_name]:-unnamed_experiment}"
 CFG="${KV[cfg]:-custom/exps/Images04.py}"
 CKPT="${KV[ckpt]:-checkpoints/yolox_x.pth}"
-DATA_DIR="${KV[data_dir]:-datasets_tar/Images05ACCV2026.tar}"
+DATA_DIR="${KV[data_dir]:-datasets_tar/Images05MetaFood2026.tar}"
 
 # ----- ENVIRONMENT SETUP -------------------------------------------
 unset SLURM_EXPORT_ENV
