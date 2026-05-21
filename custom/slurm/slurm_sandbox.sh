4e296cb8eec5e24b0d4368d1158627c047279990
Noobtoss <thomasschmittba17196@yahoo.de>
2026-05-21 10:56:54 +0200

    added sandbox

---
+++ custom/slurm/slurm_sandbox.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#SBATCH --job-name=YOLOX_train_arr # Kurzname des Jobs
+#SBATCH --array=9-9%              # 9-16%4  # 7 Jobs total running 4 at a time
+#SBATCH --output=logs/R-%A-%a.out
+#SBATCH --partition=p2,p3,p4,p5,p6             # p4
+#SBATCH --qos=preemptible  # gpuultimate
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1                  # Anzahl Knoten
+#SBATCH --ntasks=1                 # Gesamtzahl der Tasks über alle Knoten hinweg
+#SBATCH --cpus-per-task=1          # CPU Kerne pro Task (>1 für multi-threaded Tasks)
+#SBATCH --mem-per-cpu=64G          # RAM pro CPU Kern #20G #32G #64G
+
+# ----- BASE_DIR ----------------------------------------------------
+BASE_DIR=/nfs/scratch/staff/schmittth/code_nexus/YOLOX
+
+# ----- GET ARGS ----------------------------------------------------
+PARAMS_FILE="$BASE_DIR/custom/slurm/slurm_params.txt"
+PARAMS=$(grep -v '^[[:space:]]*#' "$PARAMS_FILE" | sed -n "$((SLURM_ARRAY_TASK_ID))p")
+
+# Add SLURM_ARRAY_JOB_ID and SLURM_ARRAY_TASK_ID to exp_name
+PARAMS=$(echo "$PARAMS" | sed -E "s/(exp_name[[:space:]]+[^[:space:]]+)/\1_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}/")
+declare -A KV
+read -r -a ARR <<< "$PARAMS"
+for ((i=0; i<${#ARR[@]}; i+=2)); do
+    key="${ARR[$i]}"
+    value="${ARR[$i+1]}"
+    KV["$key"]="$value"
+done
+[[ "$PARAMS" != *"seed"* ]] && PARAMS="$PARAMS seed ${SLURM_ARRAY_JOB_ID}"
+
+OUTPUT_DIR="${BASE_DIR}/tmp"
+EXP_NAME="${KV[exp_name]:-unnamed_experiment}"
+CFG="${KV[cfg]:-custom/exps/Images04.py}"
+CKPT="${KV[ckpt]:-checkpoints/yolox_x.pth}"
+
+# ----- ENVIRONMENT SETUP -------------------------------------------
+module purge
+module load python/anaconda3
+eval "$(conda shell.bash hook)"
+
+conda activate conda-YOLOX
+
+export PYTHONPATH="$BASE_DIR:$PYTHONPATH"
+
+# ----- WANDB -------------------------------------------------------
+export WANDB_API_KEY=95177947f5f36556806da90ea7a0bf93ed857d58
+export WANDB_DIR=/tmp/ths_wandb
+export WANDB_CACHE_DIR=/tmp/ths_wandb
+export WANDB_CONFIG_DIR=/tmp/ths_wandb
+
+# ----- TRAINING ----------------------------------------------------
+python tools/train.py \
+    --exp_file $BASE_DIR/$CFG \
+    --devices 1 \
+    --batch-size 8 \
+    --fp16 \
+    --occup \
+    --ckpt $BASE_DIR/$CKPT \
+    --cache \
+    --logger wandb \
+        wandb-project Images05ACCV2026-YOLOX-InsanityCheck \
+        wandb-entity team-noobtoss \
+        wandb-name $EXP_NAME \
+    output_dir $OUTPUT_DIR \
+    $PARAMS
+
+# ----- CLEANUP -----------------------------------------------------
+KEEP_FILES=("train_log.txt" "last_epoch_ckpt.pth")
+eval find "$OUTPUT_DIR/$EXP_NAME" -type f $(printf ' ! -name "%s"' "${KEEP_FILES[@]}") -delete
