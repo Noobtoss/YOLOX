369a827a0901756e038316ad0360e66a84229eaa
Noobtoss <thomasschmittba17196@gmail.com>
2026-06-15 16:15:26 +0200

    refactoring Mix -> Plus

--- custom/exps/Images05ACCV2026Plus.py
+++ custom/exps/Images05ACCV2026Plus.py
@@ -35,7 +35,7 @@ class Exp(ExpACCV2026):
         # ---------------- dataloader config ---------------- #
 
         # Define yourself dataset path
-        self.data_dir = "datasets/Images05ACCV2026Mix"
+        self.data_dir = "datasets/Images05ACCV2026Plus"
         self.train_ann = "annotation_train.json"
         self.val_ann = "annotation_test.json"
 
