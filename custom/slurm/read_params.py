6b93a11fb0ea37dcf93182ac4bb08a6b3e1ffcef
Noobtoss <thomasschmittba17196@gmail.com>
2026-06-23 17:46:28 +0200

    using

--- custom/slurm/read_params.py
+++ custom/slurm/read_params.py
@@ -26,7 +26,7 @@ def parse_experiments(text: str, task_ids: list[int]) -> list[str]:
     return experiments
 
 if __name__ == "__main__":
-    array_str = "92,93,94,95,96,97,108,109,110,111,112,113%3  # Previous runs: 17-40%8, 9-16%8"
+    array_str = "92,93,94,95,96,97,108,109,110,111,112,113%4"
     task_ids  = parse_array_ids(array_str)
 
     with open("slurm_params.txt", "r") as f:
